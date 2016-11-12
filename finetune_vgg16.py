d92a8ed576c904ecd7ed8c6e49f4c2426d3de173
Anshuman Suri <anshuman14021@iiitd.ac.in>
2016-11-12 11:41:14 +0530

    Writing code for finetuning.

--- finetune_vgg16.py
+++ finetune_vgg16.py
@@ -6,13 +6,16 @@ from keras.models import Sequential
 from keras.layers.core import Flatten, Dense, Dropout
 from keras.layers.convolutional import Convolution2D, MaxPooling2D, ZeroPadding2D
 from keras.optimizers import SGD
+from keras.callbacks import TensorBoard
 import cv2, numpy as np
 import pickle
 import sys
+import random
+import os
 
 # Source for vgg16 implementation : https://gist.github.com/baraldilorenzo/07d7802847aaad0a35d3
 
-def VGG_16(weights_path=None):
+def ready_model(weights_path, n_classes):
 	model = Sequential()
 	model.add(ZeroPadding2D((1,1),input_shape=(3,224,224)))
 	model.add(Convolution2D(64, 3, 3, activation='relu'))
@@ -58,30 +61,82 @@ def VGG_16(weights_path=None):
 	model.add(Dense(365, activation='softmax'))
 
 	model.load_weights(weights_path)
+
+	# Freeze all layers except the last two
+	model.pop()
+	model.pop()
+	model.add(Dropout(0.5))
+	model.add(Dense(n_classes, activation='softmax'))
 	sgd = SGD(lr=0.1, decay=1e-6, momentum=0.9, nesterov=True)
-	model.compile(optimizer=sgd, loss='categorical_crossentropy')
+	model.compile(optimizer=sgd, loss='categorical_crossentropy',metrics=['accuracy'])
 	return model
 
 
-def predict(model, labels, img_path):
+def process_image(img_path):
 	im = cv2.resize(cv2.imread(img_path), (224, 224)).astype(np.float32)
 	im[:,:,0] -= 103.939
 	im[:,:,1] -= 116.779
 	im[:,:,2] -= 123.68
 	im = im.transpose((2,0,1))
 	im = np.expand_dims(im, axis=0)
-	# Test pretrained model
-	out = model.predict(im)
-	out  = reversed(np.argsort(out)[0])
-	results = []
-	for x in out:
-		results.append(labels[x])
-	return results
+	return im
+
+
+def train_test_set(image_filenames, ratio = 0.8):
+	X = []
+	y = []
+	random.shuffle(image_filenames.keys())
+	for img in image_filenames.keys():
+		X.append(process_image(img))
+		y.append(image_filenames[img])
+	split_ratio = int(len(X) * ratio)
+	X_train, y_train = X[:split_ratio], y[:split_ratio]
+	X_test, y_test = X[split_ratio:], y[split_ratio:]
+	X_train, y_train =  np.array(X_train), np.array(y_train)
+	X_test, y_test =  np.array(X_test), np.array(y_test)
+	return (X_train, y_train), (X_test, y_test)
+
+
+def ready_data(base_dir):
+	# Link class names with labels
+	name_index_mapping = {}
+	index_name_mapping = {}
+	file_name_mapping = {}
+	labels = []
+	for root, subdirs, files in os.walk(base_dir):
+		labels = subdirs[:]
+		break
+	for class_index, class_name in enumerate(labels):
+		name_index_mapping[class_name] = class_index
+		index_name_mapping[class_index] = class_name
+	# Walk through directory, gathering names of images with their labels
+	n_classes = 0
+	for root, subdirs, files in os.walk(base_dir):
+		n_classes += 1
+		for filename in files:
+			file_path = os.path.join(root, filename)
+			assert file_path.startswith(base_dir)
+			suffix = file_path[len(base_dir):]
+			suffix = suffix.lstrip("/")
+			label = suffix.split("/")[0]
+			file_name_mapping[file_path] = name_index_mapping[label]
+	return file_name_mapping, n_classes
+
+
+def fine_tune(model, file_name_map):
+	n_eopchs = 10
+	batch_size = 32
+	(X_train, y_train), (X_test, y_test) = train_test_set(file_name_map)
+	model.fit(X_train,y_train,
+				nb_epoch = n_eopchs,
+				batch_size = batch_size,
+				callbacks = [TensorBoard(log_dir='/tmp/plaves_vgg16_finetune')])
+	loss_and_metrics = model.evaluate(X_test, y_test, batch_size=bs)
+	model.save("finetuned_keras.h5")
+	return loss_and_metrics * 100.0
 
 
 if __name__ == "__main__":
-	im_name = sys.argv[1]
-	model = VGG_16('places_vgg_keras.h5')
-	labels = pickle.load(open('labels.pkl','rb'))
-	z = predict(model, labels, im_name)
-	print z[:5]
+	file_name_map, n_classes = ready_data(sys.argv[1])
+	model = ready_model('places_vgg_keras.h5', n_classes)
+	print "Accuracy was %f%", fine_tune(model, file_name_map)
