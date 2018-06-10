ab30649d4437a30c6b7c07fa30eeda49fd9d2d4c
Tony Hong <humanhong1001@gmail.com>
2018-06-10 12:17:19 +0200

    chord: refactor

--- place.py
+++ place.py
@@ -1,3 +1,5 @@
+# get_features.py
+
 import tensorflow as tf
 
 import keras
@@ -14,95 +16,96 @@ config = tf.ConfigProto()
 config.gpu_options.allow_growth=True
 sess = tf.Session(config=config)
 keras.backend.set_session(sess)
-	
+    
 # Source for vgg16 implementation : https://gist.github.com/baraldilorenzo/07d7802847aaad0a35d3
 
 model = None
 labels = None
 
 def VGG_16(weights_path=None):
-	model = Sequential()
-	model.add(ZeroPadding2D((1,1),input_shape=(3,224,224)))
-	model.add(Convolution2D(64, 3, 3, activation='relu'))
-	model.add(ZeroPadding2D((1,1)))
-	model.add(Convolution2D(64, 3, 3, activation='relu'))
-	model.add(MaxPooling2D((2,2), strides=(2,2)))
+    model = Sequential()
+    model.add(ZeroPadding2D((1,1),input_shape=(3,224,224)))
+    model.add(Convolution2D(64, 3, 3, activation='relu'))
+    model.add(ZeroPadding2D((1,1)))
+    model.add(Convolution2D(64, 3, 3, activation='relu'))
+    model.add(MaxPooling2D((2,2), strides=(2,2)))
 
-	model.add(ZeroPadding2D((1,1)))
-	model.add(Convolution2D(128, 3, 3, activation='relu'))
-	model.add(ZeroPadding2D((1,1)))
-	model.add(Convolution2D(128, 3, 3, activation='relu'))
-	model.add(MaxPooling2D((2,2), strides=(2,2)))
+    model.add(ZeroPadding2D((1,1)))
+    model.add(Convolution2D(128, 3, 3, activation='relu'))
+    model.add(ZeroPadding2D((1,1)))
+    model.add(Convolution2D(128, 3, 3, activation='relu'))
+    model.add(MaxPooling2D((2,2), strides=(2,2)))
 
-	model.add(ZeroPadding2D((1,1)))
-	model.add(Convolution2D(256, 3, 3, activation='relu'))
-	model.add(ZeroPadding2D((1,1)))
-	model.add(Convolution2D(256, 3, 3, activation='relu'))
-	model.add(ZeroPadding2D((1,1)))
-	model.add(Convolution2D(256, 3, 3, activation='relu'))
-	model.add(MaxPooling2D((2,2), strides=(2,2)))
+    model.add(ZeroPadding2D((1,1)))
+    model.add(Convolution2D(256, 3, 3, activation='relu'))
+    model.add(ZeroPadding2D((1,1)))
+    model.add(Convolution2D(256, 3, 3, activation='relu'))
+    model.add(ZeroPadding2D((1,1)))
+    model.add(Convolution2D(256, 3, 3, activation='relu'))
+    model.add(MaxPooling2D((2,2), strides=(2,2)))
 
-	model.add(ZeroPadding2D((1,1)))
-	model.add(Convolution2D(512, 3, 3, activation='relu'))
-	model.add(ZeroPadding2D((1,1)))
-	model.add(Convolution2D(512, 3, 3, activation='relu'))
-	model.add(ZeroPadding2D((1,1)))
-	model.add(Convolution2D(512, 3, 3, activation='relu'))
-	model.add(MaxPooling2D((2,2), strides=(2,2)))
+    model.add(ZeroPadding2D((1,1)))
+    model.add(Convolution2D(512, 3, 3, activation='relu'))
+    model.add(ZeroPadding2D((1,1)))
+    model.add(Convolution2D(512, 3, 3, activation='relu'))
+    model.add(ZeroPadding2D((1,1)))
+    model.add(Convolution2D(512, 3, 3, activation='relu'))
+    model.add(MaxPooling2D((2,2), strides=(2,2)))
 
-	model.add(ZeroPadding2D((1,1)))
-	model.add(Convolution2D(512, 3, 3, activation='relu'))
-	model.add(ZeroPadding2D((1,1)))
-	model.add(Convolution2D(512, 3, 3, activation='relu'))
-	model.add(ZeroPadding2D((1,1)))
-	model.add(Convolution2D(512, 3, 3, activation='relu'))
-	model.add(MaxPooling2D((2,2), strides=(2,2)))
+    model.add(ZeroPadding2D((1,1)))
+    model.add(Convolution2D(512, 3, 3, activation='relu'))
+    model.add(ZeroPadding2D((1,1)))
+    model.add(Convolution2D(512, 3, 3, activation='relu'))
+    model.add(ZeroPadding2D((1,1)))
+    model.add(Convolution2D(512, 3, 3, activation='relu'))
+    model.add(MaxPooling2D((2,2), strides=(2,2)))
 
-	model.add(Flatten())
-	model.add(Dense(4096, activation='relu'))
-	model.add(Dropout(0.5))
-	model.add(Dense(4096, activation='relu'))
-	model.add(Dropout(0.5))
-	model.add(Dense(365, activation='softmax'))
+    model.add(Flatten())
+    model.add(Dense(4096, activation='relu', name='dense1'))
+    model.add(Dropout(0.5))
+    model.add(Dense(4096, activation='relu', name='dense2'))
+    model.add(Dropout(0.5))
+    model.add(Dense(365, activation='softmax'))
 
-	model.load_weights(weights_path)
-	sgd = SGD(lr=0.1, decay=1e-6, momentum=0.9, nesterov=True)
-	model.compile(optimizer=sgd, loss='categorical_crossentropy')
-	return model
+    model.load_weights(weights_path)
+    sgd = SGD(lr=0.1, decay=1e-6, momentum=0.9, nesterov=True)
+    model.compile(optimizer=sgd, loss='categorical_crossentropy')
+    return model
 
 
 def predict(img_path, ret_dict):
-	global model
-	global labels
-	im = cv2.resize(cv2.imread(img_path), (224, 224)).astype(np.float32)
-	im[:,:,0] -= 103.939
-	im[:,:,1] -= 116.779
-	im[:,:,2] -= 123.68
-	im = im.transpose((2,0,1))
-	im = np.expand_dims(im, axis=0)
-	# Test pretrained model
-	out = model.predict(im)
-	out  = reversed(np.argsort(out)[0])
-	results = []
-	for x in out:
-		results.append(labels[x])
-	ret_dict['place'] = results[:5]
-	return results[:5]
+    global model
+    global labels
+    im = cv2.resize(cv2.imread(img_path), (224, 224)).astype(np.float32)
+    im[:,:,0] -= 103.939
+    im[:,:,1] -= 116.779
+    im[:,:,2] -= 123.68
+    im = im.transpose((2,0,1))
+    im = np.expand_dims(im, axis=0)
+    # Test pretrained model
+    out = model.predict(im)
+    out  = reversed(np.argsort(out)[0])
+    results = []
+    for x in out:
+        results.append(labels[x])
+    ret_dict['place'] = results[:5]
+    return results[:5]
 
 
 def init():
-	global model
-	global labels
-	keras.backend.set_image_dim_ordering('th')
-	model = VGG_16('models/places/places_vgg_keras.h5')
-	labels = pickle.load(open('models/places/labels.pkl','rb'))
+    global model
+    global labels
+    keras.backend.set_image_dim_ordering('th')
+    model = VGG_16('models/places/places_vgg_keras.h5')
+    labels = pickle.load(open('models/places/labels.pkl','rb'))
 
 
 init()
 print "Loaded place module"
 
 if __name__ == "__main__":
-	im_name = sys.argv[1]
-	ret_dict = {}
-	z = predict(im_name, ret_dict)
-	print z[:5]
+    im_name = sys.argv[1]
+    ret_dict = {}
+    z = predict(im_name, ret_dict)
+
+    print z[:5]
